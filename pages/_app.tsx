2da3eac5c68ae000911142b734ad9fb3201f6394
Jens Munch <jens.a.munch@gmail.com>
2021-02-14 02:23:15 +0000

    fix for theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
-import { extendTheme } from '@chakra-ui/react'
-
-const config = {
-  initialColorMode: 'light',
-  useSystemColorMode: false,
-}
-
-const theme = extendTheme({ config })
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider resetCSS>
       <Component {...pageProps} />
     </ChakraProvider>
   )
