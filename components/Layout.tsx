33943bac8370606a8d98cd473608321be26e553b
Jens Munch <jens.a.munch@gmail.com>
2021-03-14 12:13:34 +0000

    header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import Header from './Header'
+import Header2 from './Header2'
 import { Box, Flex } from '@chakra-ui/react'
 import Head from 'next/head'
 
@@ -17,7 +17,7 @@ const Layout: React.FC<Props> = (props) => (
     </Head>
     <Flex align="center" justify="center">
       <Box w="1200px" m="4">
-        <Header />
+        <Header2 />
         {props.children}
       </Box>
     </Flex>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
 
+import customTheme from "../utils/theme";
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={customTheme}>
       <Provider session={pageProps.session}>
         <Component {...pageProps} />
       </Provider>
