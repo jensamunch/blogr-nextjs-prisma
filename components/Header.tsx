f2a13d67fb015f05e79708f53624b3dd6a1b2de3
Jens Munch <jens.a.munch@gmail.com>
2021-03-15 14:49:31 +0000

    fix spinner

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
 // Header.tsx
 import React from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { Router, useRouter } from 'next/router'
 import { signOut, useSession } from 'next-auth/client'
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
 
 const Header: React.FC = () => {
   const router = useRouter()
