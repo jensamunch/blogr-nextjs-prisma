876d267d2c3318e468753104270b8ef1b44cc9b3
Jens Munch <jens.a.munch@gmail.com>
2021-03-15 00:22:13 +0000

    ok done

--- pages/my-posts.tsx
+++ pages/my-posts.tsx
@@ -36,6 +36,24 @@ type Props = {
 }
 
 const MyPosts: React.FC<Props> = (props) => {
+  const [loading, setLoading] = React.useState(false)
+  React.useEffect(() => {
+    const start = () => {
+      setLoading(true)
+    }
+    const end = () => {
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
+
   const [session] = useSession()
 
   async function deletePost(id: number): Promise<void> {
@@ -94,26 +112,56 @@ const MyPosts: React.FC<Props> = (props) => {
           <div key={post.id} className="mx-12">
             <Post post={post} />
             <button
-              className="mt-5 mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+              className="mt-5 mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded inline-flex"
               onClick={() => deletePost(post.id)}
+              disabled={loading ? true : false}
             >
-              Delete{' '}
+              Delete
             </button>
             <button
-              className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+              className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded inline-flex"
               onClick={() => publishPost(post.id)}
+              disabled={loading ? true : false}
             >
-              Publish{' '}
+              Publish
             </button>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded inline-flex"
               onClick={() => unPublishPost(post.id)}
+              disabled={loading ? true : false}
             >
               Unpublish
             </button>
           </div>
         ))}
       </div>
+
+      {loading ? (
+        <div id="loading-screen" className="absolute top-0 left-0 h-screen w-screen flex items-center justify-center bg-gray-900 bg-opacity-50">
+          <svg
+            className=" animate-spin h-24 w-24 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      ) : (
+        ''
+      )}
     </Layout>
   )
 }
